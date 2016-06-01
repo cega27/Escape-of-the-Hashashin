800dd70ac40feda99b43189c31c9987c156af3fc
Garen <Garen@10.18.148.79>
2016-06-01 13:43:50 -0600

    6/1/16 Starting the view layer

--- escapeHashashin/src/byui/cit260/escapeHashashin/view/StartProgramView.java
+++ escapeHashashin/src/byui/cit260/escapeHashashin/view/StartProgramView.java
@@ -11,4 +11,24 @@ package byui.cit260.escapeHashashin.view;
  */
 public class StartProgramView {
     
-}
+        private String promptMessage;
+    
+        public StartProgramView() {
+         // promptMessage = "please enter your name"
+         // display the banner when view is created
+         this.promptMessage = "\nPlease enter your name";
+         //display the banner when view is created
+         this.displayBanner();
+        }
+
+    private void displayBanner() {
+        System.out.println("\nWelcome to your world as an Assassin. Within history there have been"
+                + "many assassins and this is your chance to partake in the killing. You are a"
+                + "Hashashin. Hashashin is the world that our modern term assassin stems from. "
+                + "Solve puzzles and escape the castle without being detected. Complete with the"
+                + "most efficent and fastest time. Good Luck.");
+    }
+        }
+    
+
+
